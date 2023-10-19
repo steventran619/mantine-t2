5a6f62b5f91677bfbf47ebe6106a47b3d05c14b3
Steven Tran <dpoopd@gmail.com>
2023-10-19 12:26:53 -0700

    Updated base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mantine-t2/'
+  base: 'https://steventran619.github.io/mantine-t2/'
 })
